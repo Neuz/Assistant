ed5bc9c3c1fcaaa6f087f1cefd0e339fe7d7092b
viacooky <viacooky@qq.com>
2022-07-07 20:44:03 +0800

    feat: 增加 ico ，调整样式

--- src/Assistant/ViewModel/SystemInfoViewModel.cs
+++ src/Assistant/ViewModel/SystemInfoViewModel.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using CZGL.SystemInfo;
+﻿using CZGL.SystemInfo;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
 
 namespace Assistant.ViewModel;
 
@@ -9,24 +9,28 @@ public class SystemInfoViewModel : ObservableObject
 {
     public SystemInfoViewModel()
     {
+        var ts = TimeSpan.FromMilliseconds(Environment.TickCount);
+        SystemPlatformDict = new Dictionary<string, object>
+        {
+            {"操作系统", SystemPlatformInfo.OSDescription},
+            {"操作系统版本", SystemPlatformInfo.OSVersion},
+            {"操作系统类型", SystemPlatformInfo.OSPlatformID},
+            {"平台架构", SystemPlatformInfo.OSArchitecture},
+            {"计算机名称	", SystemPlatformInfo.MachineName},
+            {"当前用户名称", SystemPlatformInfo.UserName},
+            {"用户网络域名称", SystemPlatformInfo.UserDomainName},
+            {"系统目录", SystemPlatformInfo.SystemDirectory},
+            {"系统运行时间", $"{ts.Days}天{ts.Hours}小时{ts.Minutes}分{ts.Seconds}秒"},
+            {"框架平台", SystemPlatformInfo.FrameworkDescription},
+            {"框架版本", SystemPlatformInfo.FrameworkVersion},
+            {"CPU核心数", SystemPlatformInfo.ProcessorCount},
+            {"CPU架构", SystemPlatformInfo.ProcessArchitecture}
+        };
     }
-    
+
+    public NetworkInfo[] NetworkInfos { get; } = NetworkInfo.GetRealNetworkInfos();
+
     public DiskInfo[] DiskInfos { get; } = DiskInfo.GetDisks();
 
-    public Dictionary<string, object> SystemPlatformDict { get; }= new()
-    {
-        {"操作系统", SystemPlatformInfo.OSDescription},
-        {"操作系统版本", SystemPlatformInfo.OSVersion},
-        {"操作系统类型", SystemPlatformInfo.OSPlatformID},
-        {"平台架构", SystemPlatformInfo.OSArchitecture},
-        {"计算机名称	", SystemPlatformInfo.MachineName},
-        {"当前用户名称", SystemPlatformInfo.UserName},
-        {"用户网络域名称", SystemPlatformInfo.UserDomainName},
-        {"系统目录", SystemPlatformInfo.SystemDirectory},
-        {"框架平台", SystemPlatformInfo.FrameworkDescription},
-        {"框架版本", SystemPlatformInfo.FrameworkVersion},
-        {"CPU核心数", SystemPlatformInfo.ProcessorCount},
-        {"CPU架构", SystemPlatformInfo.ProcessArchitecture},
-    };
-    
+    public Dictionary<string, object> SystemPlatformDict { get; }
 }
