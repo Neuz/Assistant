f7ebfe25e9c2c0423c69eafab0499145f61fbc59
viacooky <viacooky@qq.com>
2022-07-11 01:43:23 +0800

    feat: 代码优化

---
+++ src/Assistant/Model/ToolModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Assistant.Model;
+
+public class ToolModel
+{
+    public string? Name { get; set; }
+    public Geometry? Icon { get; set; }
+    public string? IconColor { get; set; }
+    public string? Description { get; set; }
+    public string? BinPath { get; set; }
+
+    public Action<ToolModel>? RunAction { get; set; }
+    public Action<ToolModel>? HelpAction { get; set; }
+}
--- src/Assistant/ViewModel/ToolsViewModel.cs
+++ src/Assistant/ViewModel/ToolsViewModel.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Assistant.Model;
+﻿using Assistant.Model;
 using CliWrap;
 using CliWrap.Buffered;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Assistant.Utils;
 
 namespace Assistant.ViewModel;
 
@@ -25,22 +22,44 @@ public class ToolsViewModel : ObservableObject
 
     private async Task HelpHandler(object? arg)
     {
-        if (arg?.ToString() == "BeekeeperStudio")
-            await Cli.Wrap("cmd.exe")
-                     .WithArguments(new[] {"/c", "start", "https://www.beekeeperstudio.io/"})
-                     .WithValidation(CommandResultValidation.None)
-                     .ExecuteBufferedAsync();
+        if (arg is ToolModel model) await Task.Run(() => model.HelpAction?.Invoke(model));
     }
 
     private async Task RunHandler(object? arg)
     {
-        if (arg?.ToString() == "BeekeeperStudio")
-        {
-            var path = Path.Combine(Global.CurrentDir, "Tools", "BeekeeperStudio", "BeekeeperStudio.exe");
-            await Cli.Wrap(path).ExecuteAsync();
-        }
+        if (arg is ToolModel model) await Task.Run(() => model.RunAction?.Invoke(model));
     }
 
     public ICommand RunCommand { get; set; }
     public ICommand HelpCommand { get; set; }
+
+    public ToolModel[] Tools { get; } =
+    {
+        new()
+        {
+            Name        = "Beekeeper Studio",
+            Icon        = Application.Current.FindResource("Svg.BeekeeperStudio") as Geometry,
+            IconColor   = "#FAD83B",
+            Description = "Open Source SQL Editor and Database Manager A modern, easy to use, and good looking SQL client for MySQL, Postgres, SQLite, SQL Server, and more.",
+            BinPath     = Path.Combine(Global.CurrentDir, "Tools", "BeekeeperStudio", "BeekeeperStudio.exe"),
+            RunAction =  model =>
+            {
+                if (!string.IsNullOrEmpty(model.BinPath)) Cli.Wrap(model.BinPath).ExecuteAsync();
+            },
+            HelpAction = obj => { _ = FileUtils.OpenUrl("https://www.beekeeperstudio.io/"); }
+        },
+        new()
+        {
+            Name        = "远程桌面连接",
+            Icon        = Application.Current.FindResource("Svg.RemoteDesktop") as Geometry,
+            IconColor   = "#FAD83B",
+            Description = "微软远程桌面控制",
+            BinPath     = "mstsc.exe",
+            RunAction = model =>
+            {
+                if (!string.IsNullOrEmpty(model.BinPath)) Cli.Wrap(model.BinPath).ExecuteAsync();
+            },
+            HelpAction = obj => { _ = FileUtils.OpenUrl("https://go.microsoft.com/fwlink/?LinkId=528886"); }
+        }
+    };
 }
