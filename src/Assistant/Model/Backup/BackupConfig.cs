cb927ec019e6628e52233dc82235f0da77eef0ed
viacooky <viacooky@qq.com>
2022-08-30 16:08:27 +0800

    feat: 重构MySQL备份功能的结构

---
+++ src/Assistant/Model/Backup/BackupConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assistant.Model.Backup;
+
+public class BackupConfig
+{
+    public string Name { get; set; }
+
+    public DateTime CreateTime { get; set; }
+    public DateTime ModifyTime { get; set; }
+    public string Note { get; set; }
+
+
+    public string Host { get; set; } = "127.0.0.1";
+    public int Port { get; set; } = 10001;
+    public string UserID { get; set; } = "root";
+    public string Password { get; set; } = "Neuz123";
+
+    public string Database { get; set; }
+
+    /// <summary>
+    /// 导出的表的列表。如果没有，则将导出所有表
+    /// </summary>
+    public List<string> Tables { get; set; } = new();
+
+    /// <summary>
+    /// 是否导出存储过程
+    /// </summary>
+    public bool ExportProcedures { get; set; }
+    /// <summary>
+    /// 是否应导出存储的函数
+    /// </summary>
+    public bool ExportFunctions { get; set; }
+    /// <summary>
+    /// 是否应导出存储的触发器
+    /// </summary>
+    public bool ExportTriggers { get; set; }
+    /// <summary>
+    /// 是否应导出存储视图
+    /// </summary>
+    public bool ExportViews { get; set; }
+    /// <summary>
+    /// 是否应导出存储的事件
+    /// </summary>
+    public bool ExportEvents { get; set; }
+}
---
+++ src/Assistant/Model/Backup/BackupFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assistant.Model.Backup;
+
+public class BackupFile
+{
+    public string Name { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+    public string Note { get; set; } = string.Empty;
+    public DateTime CreateTime { get; set; } = DateTime.MinValue;
+    public DateTime ModifyTime { get; set; } = DateTime.MinValue;
+    public string FileSize { get; set; } = string.Empty;
+}
